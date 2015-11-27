031ccba792ca69f49ac6f0389aab72aa899d80cd
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-11-27 13:41:01 +0000

    removed data analysis scripts not relevant for main code and renamed main script

--- remove_cen.sh
+++
@@ -1,9 +0,0 @@
-
-for i in {1..5} 
-do
-	ruby remove_cent.rb $i Aw_sup1-2/filter2_chromosome$i
-	ruby remove_cent.rb $i BCF2/BCF2_chromosome$i
-	ruby remove_cent.rb $i OCF2/OCF2_chromosome$i
-	ruby remove_cent.rb $i B/B_chromosome$i
-	ruby remove_cent.rb $i C/C_chromosome$i
-done
