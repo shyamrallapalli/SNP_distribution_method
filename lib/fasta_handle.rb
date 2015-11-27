178de8ea39ae1ae399e2ee485049f89a3ef69342
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-11-27 17:14:25 +0000

    output.rb method moved to fasta_handle

--- lib/fasta_handle.rb
+++ lib/fasta_handle.rb
@@ -17,25 +17,25 @@ class FastaHandle
     return ids, lengths, id_len
   end
 
-	# Input: FASTA file
-	# Output: Array of Bio::FastaFormat entries
-	def self.fasta_array(fasta_file)
-		fasta = [] # we have the lengths of each fasta, but the frags are different to those of the vcf/hash(this only has the frags w snps)
-		Bio::FastaFormat.open(fasta_file).each do |i| # get array of fasta format frags, ##  WE NEED TO REORDER THE FASTA FRAGS HERE, TO TEST DIFFERENT ARRANGEMENTS
-			fasta << i
-		end
-		return fasta
-	end
+  # Input: FASTA file
+  # Output: Array of Bio::FastaFormat entries
+  def self.fasta_array(fasta_file)
+    fasta = [] # we have the lengths of each fasta, but the frags are different to those of the vcf/hash(this only has the frags w snps)
+    Bio::FastaFormat.open(fasta_file).each do |i| # get array of fasta format frags, ##  WE NEED TO REORDER THE FASTA FRAGS HERE, TO TEST DIFFERENT ARRANGEMENTS
+      fasta << i
+    end
+    return fasta
+  end
 
   # Input: FASTA file
-	# Output: Integer of the length of the genome
-	def self.genome_length(fasta_file)
-		lengths = []
-		fasta_array(fasta_file).each do |frag|
-			lengths << frag.length
-		end
-		return lengths.inject(:+)
-	end
+  # Output: Integer of the length of the genome
+  def self.genome_length(fasta_file)
+    lengths = []
+    fasta_array(fasta_file).each do |frag|
+      lengths << frag.length
+    end
+    return lengths.inject(:+)
+  end
 
   # Input: FASTA file
   # Output: hash of sequence ids with lengths and seqeunces and total assmebly length
@@ -48,6 +48,17 @@ class FastaHandle
       assembly_len += inseq.length
     end
     return sequences, assembly_len
+  end
+
+  # Input1: permutation array of frag ids after SDM
+  # Input2: Fasta file seq hash with ids as keys
+  # Output: permutation of fragments after SDM with the data (lengths, etc) obtained from the original fasta file
+  def self.create_perm_fasta(perm, frags)
+    fasta_perm = []
+    perm.each do |frag|
+      fasta_perm << Bio::FastaFormat.new(frags[frag].to_s) if frags.key?(frag)
+    end
+    fasta_perm
   end
 
 end
