0cf23c12f71479b821f77893f4be16fd5d4ce36e
Shyam Rallapalli <shyam.rallapalli@gmail.com>
2015-12-16 14:46:14 +0000

    test for Fragments methods

--- Test/unit_tests_fragments.rb
+++ Test/unit_tests_fragments.rb
@@ -4,31 +4,23 @@ require_relative '../lib/fragments'
 
 class TestFragments < Test::Unit::TestCase
   def setup
-    @dic_ratio_inv = {11.374979 => ['a'], 11.842904 => ['b'], 10.120768 => ['c'], 5.022447 => ['d', 'e', 'f', 'g', 'h', 'i', 'j']}
-    @dic_hm_inv = {11.374979 => ['a'], 11.842904 => ['b'], 10.120768 => ['c'], 5.022447 => ['d', 'e', 'f', 'g', 'h', 'i', 'j']}
-    @dic_pos_hm = {'a' => [1], 'b' => [2], 'c' => [3, 4], 'd' => [5, 6], 'e' => [7], 'f' => [10], }
+    @dic_ratios = {11.374979=>['a'], 11.842904=>['b'], 10.120768=>['c'], 5.022447=>%w(d e f g h i j)}
   end
 
   def test_split
-    inkeys = @dic_hm_inv.keys
-    right, left, inkeys = Fragments.split(@dic_hm_inv, right = [], left = [], inkeys, 0)
-    assert_equal( ['e', 'f', 'g', 'd'], right)
-    assert_equal( ['h', 'i', 'j'], left)
-    assert_equal( [11.374979, 11.842904, 10.120768], inkeys)
-  end
-
-  def test_sort
-    perm_back, mut_back = Fragments.sort(@dic_hm_inv, cross = 'back', average_contig = 2500)
-    assert_equal(['e', 'd', 'a', 'b', 'c', 'f'], perm_back)
-    assert_equal(['e', 'd', 'a', 'b', 'c', 'f'], mut_back)
+    left = []
+    right = []
+    inkeys = @dic_ratios.keys
+    left, right, inkeys = Fragments.split(@dic_ratios, left, right, inkeys, 0)
+    assert_equal(['d', %w(e f g)], left)
+    assert_equal([%w(h i j)], right)
+    assert_equal([11.374979, 11.842904, 10.120768], inkeys)
   end
 
   def test_arrange
-    perm_hm, perm_ratio, mut, hyp = Fragments.arrange(@dic_hm_inv, @dic_ratio_inv, @dic_pos_hm, cross = 'back', average_contig = 2500)
-    assert_equal(['e', 'd', 'a', 'b', 'c', 'f'], perm_hm)
-    assert_equal(['e', 'd', 'a', 'b', 'c', 'f'], perm_ratio)
-    assert_equal(['e', 'd', 'a', 'b', 'c', 'f'], mut)
-    assert_equal([1, 2, 3, 4, 5, 6, 7, 10], hyp)
+    perm_ratio, mut = Fragments.arrange(@dic_ratios, cross = 'back', average_contig = 2500)
+    assert_equal(%w(d e f g a b c j i h), perm_ratio)
+    assert_equal(%w(d e f g a b c j i h), mut)
   end
 
 end
