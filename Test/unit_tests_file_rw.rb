f85d17c449d7df93ab9f038b48d13262ebacb2ee
Shyam Rallapalli <shyam.rallapalli@gmail.com>
2015-12-16 13:11:53 +0000

    tests for FileRW methods

--- Test/unit_tests_file_rw.rb
+++ Test/unit_tests_file_rw.rb
@@ -5,27 +5,49 @@ require 'test/unit'
 class TestFileRW < Test::Unit::TestCase
 
   def setup
-    @file = 'test/ratio_values.txt'
-    @vcf_file = 'test/test.vcf'
-    @fasta_file = 'test/test.fasta'
-    infasta = "test/test2.fasta"
-    @sequences, @total_length = FileRW.fasta_parse(infasta)
+    @file = 'data/fragments.txt'
+    @fragments = %w(frag1 frag2 frag3)
+    infasta = 'data/test2.fasta'
+    @sequences = FileRW.fasta_parse(infasta)
+  end
+
+  def test_to_array
+    fragments = FileRW.to_array(@file)
+    assert_equal(fragments, @fragments)
+  end
+
+  def test_write
+    filename = 'data/array_file.txt'
+    FileRW.write(filename, @fragments)
+    frags = FileRW.to_array(filename)
+    assert_equal(frags, @fragments)
+    %x[rm data/array_file.txt]
+  end
+
+  def test_write_txt
+    filename = 'data/array_file'
+    FileRW.write_txt(filename, @fragments)
+    frags = FileRW.to_array("#{filename}.txt")
+    assert_equal(frags, @fragments)
+    %x[rm "#{filename}.txt"]
   end
 
   def test_fasta_parse
-    assert_equal(@sequences[:seq], {"frag1" => ">frag1 Length = 8\nCCAAATAC\n", "frag2" => ">frag2 Length = 8\nGCAATCGG\n", "frag3" => ">frag3 Length = 7\nACGACAC\n"})
-    assert_equal(@sequences[:len], {"frag1" => 8, "frag2" => 8, "frag3" => 7})
-    assert_equal(@total_length, 23)
+    assert_equal(@sequences[:seq], {"frag1"=>">frag1 Length = 8\nCCAAATAC\n", "frag2"=>">frag2 Length = 8\nGCAATCGG\n", "frag3"=>">frag3 Length = 7\nACGACAC\n"})
+    assert_equal(@sequences[:len], {"frag1"=>8, "frag2"=>8, "frag3"=>7})
   end
 
   def test_write_order
-    ids = ['frag1', 'frag2', 'frag3']
-    filename = "test/test3.fasta"
-    perm = ["frag1", "frag3", "frag2"]
-    fasta_perm = FileRW.write_order(perm, @sequences[:seq], filename)
-    seqs, total_len = FileRW.fasta_parse("test/test3.fasta")
-    assert_equal(seqs[:seq], {"frag1" => ">frag1 Length = 8\nCCAAATAC\n", "frag3" => ">frag3 Length = 7\nACGACAC\n", "frag2" => ">frag2 Length = 8\nGCAATCGG\n"})
-    assert_equal(seqs[:len], {"frag1" => 8, "frag3" => 7, "frag2" => 8})
+    filename = 'data/test3.fasta'
+    perm = %w(frag1 frag3 frag2)
+    FileRW.write_order(perm, @sequences[:seq], filename)
+    seqs = FileRW.fasta_parse('data/test3.fasta')
+    assert_equal(seqs[:seq], {"frag1"=>">frag1 Length = 8\nCCAAATAC\n", "frag3"=>">frag3 Length = 7\nACGACAC\n", "frag2"=>">frag2 Length = 8\nGCAATCGG\n"})
+    assert_equal(seqs[:len], {"frag1"=>8, "frag3"=>7, "frag2"=>8})
+    %x[rm data/test3.fasta]
   end
 
 end
+
+
+
