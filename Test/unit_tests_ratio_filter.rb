30b7bb184ae9c37859be53717cd92e617c0c4cdd
Shyam Rallapalli <shyam.rallapalli@gmail.com>
2015-12-16 16:33:08 +0000

    tests and data for RatioFilter methods

--- Test/unit_tests_ratio_filter.rb
+++ Test/unit_tests_ratio_filter.rb
@@ -1,18 +1,44 @@
-require 'test/unit'
+#encoding: utf-8
 require_relative '../lib/ratio_filter'
+require 'test/unit'
+require 'yaml'
 
 class TestRatioFilter < Test::Unit::TestCase
+
+  def setup
+    @frag_hash = YAML.load_file('data/frags_hash.yml')
+  end
+
+  def test_get_ratios
+    ratios = RatioFilter.get_ratios(@frag_hash)
+    assert_kind_of(Array, ratios)
+    assert_equal([0.3333333333333333,
+                  0.3333333333333333,
+                  0.42857142857142855,
+                  3.0,
+                  3.0,
+                  1.0,
+                  0.38461538461538464],
+                 ratios)
+  end
+
+  def test_ratio_hash
+    ratios_hash = RatioFilter.ratio_hash(@frag_hash)
+    assert_kind_of(Hash, ratios_hash)
+    assert_equal({0.3333333333333333=>%w(frag68 frag49),
+                  0.38461538461538464=>['frag1'],
+                  0.42857142857142855=>['frag46'],
+                  1.0=>['frag64'],
+                  3.0=>%w(frag12 frag39)},
+                 ratios_hash)
+  end
+
 	def test_selected_ratios
-		snp_hm = [0, 14, 20, 2]
-		snp_ht = [5, 4, 2, 5]
-		threshold = 0
-		adjust = 1
-		ids = ["frag1", "frag2", "frag3", "frag4"]
-		dic_ratios, ratios = RatioFilter.selected_ratios(snp_hm, snp_ht, ids, threshold, adjust)
-		assert_kind_of(Hash, dic_ratios)
-		assert_kind_of(Array, ratios)
-		assert_equal(dic_ratios, {"frag1" => 1/6.to_f, "frag2" => 3.to_f, "frag3" => 7.to_f, "frag4" => 1/2.to_f})
-		assert_equal(ratios, [1/6.to_f, 3.to_f, 7.to_f, 1/2.to_f])
+		threshold = 20
+		ratios_hash = RatioFilter.selected_ratios(@frag_hash, threshold)
+		assert_kind_of(Hash, ratios_hash)
+		assert_equal({3.0=>%w(frag12 frag39), 1.0=>['frag64']},
+                 ratios_hash)
 	end
 
 end
