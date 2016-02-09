f55601ba20c307a3bbf8fe44a91ac0cc4c23cac8
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-02-09 11:22:40 +0000

    made a function to call pileup and walk back for position close to indels

--- lib/pileup.rb
+++ lib/pileup.rb
@@ -12,17 +12,45 @@ class Pileup
     keyfrags.each do | selfrag |
       positions = input_frags[selfrag][:hm_pos]
       positions.each do | mutpos |
-        bam.mpileup(:r => "#{selfrag}:#{mutpos}-#{mutpos}", :Q => 15, :q => 20) do |pileup|
-          ratio = 0
-          if pileup.is_snp?(:ignore_reference_n => true, :min_depth => 6, :min_non_ref_count => 3)
-            # if defined? pileup.non_ref_count
-            ratio = 1.0 - (pileup.ref_count/pileup.coverage)
-          end
+        pileups = get_pileup(bam,selfrag,mutpos)
+        if pileups.empty?
+          next
+        end
+        pileup = pileups[0]
+        if pileup.is_snp?(:ignore_reference_n => true, :min_depth => 6, :min_non_ref_count => 3)
+          # ratio = 1.0 - (pileup.ref_count/pileup.coverage)
+          ratio = pileup.non_ref_count/pileup.coverage
           sortfrags[ratio][selfrag][mutpos] = pileup
         end
       end
     end
     sortfrags
+  end
+
+  # a function to get pileup using a bam object and sequence information
+  # if the pileup is giving zero in the information there is insertion or deletion upstream of it
+  # there seems to be a location difference between vcf files and mpileup output
+  # so moving upstream in position to get these var info
+  # and not going further than 10bp upstream (10bp is arbitrary and this needs verification)
+  def self.get_pileup(bamobject,id,pos1, bq=15, mq=20)
+    # a variable to store initial position of var
+    initial_pos = pos1
+    pileuparray = []
+    bamobject.mpileup(:r => "#{id}:#{pos1}-#{pos1}", :Q => bq, :q => mq) do |pileup|
+      pileuparray << pileup
+    end
+    # calculate difference between var position and adjusted positions
+    # and check if the pileup information has zeros and change position info
+    # to get upstream position pileup information
+    pos_diff = initial_pos - pos1
+    if pileuparray[0].to_s =~ /^\t0/ and pos_diff <= 10
+      pos1 = pos1 - 1
+      pileuparray = get_pileup(bamobject,id,pos1)
+    # if the difference is larger than 10 bp then set up pileup information as null
+    elsif pileuparray[0].to_s =~ /^\t0/ and pos_diff > 10
+      pileuparray = []
+    end
+    pileuparray
   end
 
 end
