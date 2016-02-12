e6530e4f474563dca615dd5a8619e445e9aa977e
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-02-12 09:20:41 +0000

    initial commit to polyploid vcf handling and test script

---
+++ lib/poly_vcf.rb
@@ -0,0 +1,137 @@
+# encoding: utf-8
+require 'bio'
+require 'bio-samtools'
+require 'bio-gngm'
+
+class Polyploid
+
+  def self.vars_in_file(vcf_file, bamfile, fastafile)
+    # hash of frag ids with respective variant positions and their base hash info
+    # only snps have base hash info and indels base hash is empty
+    vars_hash = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+
+    # open bam object to access pileup information
+    bam = Bio::DB::Sam.new(:bam=>bamfile, :fasta=>fastafile)
+    bam.open
+
+    # read vcf file and process each variant
+    File.open(vcf_file, 'r').each do |line|
+      next if line =~ /^#/
+      v = Bio::DB::Vcf.new(line)
+      if v.variant?
+        pileups = Pileup.get_pileup(bam,v.chrom,v.pos)
+        if pileups.empty?
+          next
+        end
+        pileup = pileups[0]
+        basehash = Pileup.read_base_hash(pileup)
+        vars_hash[v.chrom][v.pos] = basehash
+      end
+    end
+    vars_hash
+  end
+
+  def self.get_base_freq(hash, depth, noise)
+    snp_hash = {}
+    coverage = hash.values.inject { | sum, n | sum + n.to_f }
+    if coverage < depth
+      return snp_hash
+    end
+
+    hash.each_key do | base |
+      next if base == :ref
+      freq = data[base].to_f/coverage
+      next if freq <= noise
+      snp_hash[base] = freq
+    end
+    snp_hash
+  end
+
+  def self.var_mode(ratio, ht_low = 0.10, ht_high = 0.90)
+    mode = ''
+    if ratio.between?(ht_low, ht_high)
+      mode = :het
+    elsif ratio > ht_high
+      mode = :hom
+    end
+    mode
+  end
+
+  def self.filter_vars(vars_hash_mut, vars_hash_bg, depth=6, noise=0.1)
+    vars_hash = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+    vars_hash_mut.each_key do | frag |
+      positions = vars_hash_mut[frag].keys
+      positions.each do | pos |
+        data1 = vars_hash_mut[frag][pos]
+        # background bulk has variant at same position
+        if vars_hash_bg[frag].key?(pos)
+          data2 = vars_hash_bg[frag][pos]
+          if data1.instance_of? Hash
+            mut_bases = get_base_freq(data1, depth, noise)
+            reps = mut_bases.length
+            if data2.instance_of? Hash
+              bg_bases = get_base_freq(data2, depth, noise)
+              if reps == 1
+                base = mut_bases.keys[0]
+                if bg_bases.key?(base)
+                  mut_type = var_mode(mut_bases[base])
+                  bg_type = var_mode(bg_bases[base])
+                  if mut_type == bg_type
+                    next
+                  else
+                    vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+                  end
+                else
+                  mut_type = var_mode(mut_bases[base])
+                  vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+                end
+              else
+                warn "#{frag}\t#{pos}\t#{mut_bases}\t#{bg_bases}\n"
+              end
+            else
+              if reps == 1
+                mut_type = var_mode(mut_bases[mut_bases.keys[0]])
+                vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+              else
+                warn "#{frag}\t#{pos}\t#{mut_bases}\t#{bg_bases}\n"
+              end
+            end
+          elsif data1.instance_of? String
+            mut_ratio = Pileup.get_nonref_ratio(data1)
+            mut_type = var_mode(mut_ratio)
+            if data2.instance_of? String
+              bg_ratio = Pileup.get_nonref_ratio(data2)
+              bg_type = var_mode(bg_ratio)
+              if mut_type == bg_type
+                next
+              else
+                vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+              end
+            else
+              vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+            end
+          end
+        # only mut bulk has variant at this position
+        else
+          # var is snp
+          if data1.instance_of? Hash
+            mut_bases = get_base_freq(data1, depth, noise)
+            if mut_bases.length == 1
+              mut_type = var_mode(mut_bases[mut_bases.keys[0]])
+              vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+            else
+              warn "#{frag}\t#{pos}\t#{mut_bases}\n"
+            end
+          # var is indel
+          elsif data1.instance_of? String
+            mut_ratio = Pileup.get_nonref_ratio(data1)
+            mut_type = var_mode(mut_ratio)
+            vars_hash = Vcf.push_to_hash(vars_hash, frag, pos, mut_type)
+          end
+        end
+      end
+    end
+    vars_hash_mut
+  end
+
+end
